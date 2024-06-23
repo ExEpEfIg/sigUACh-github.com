abcda9a55705b219b5a86a9ce9e94a5422f1cd8d
ExEpEfIg <168126448+ExEpEfIg@users.noreply.github.com>
2024-06-22 22:58:56 -0400

    Add files via upload

---
+++ cuencaindex.py
@@ -0,0 +1,160 @@
+
+#Función que añade a archivo vectorial información morfométrica y topográfica
+#se requiere DEM
+
+
+def cuencaindex(dem,cuencaInput,morfoCuenca):
+
+    import processing   
+    
+    #Entradas
+    dem=r"C:\Users\exequ\Documents\Contenedor_Scripts\DEM.tif"
+    cuencaInput=r"C:\Users\exequ\Documents\Contenedor_Scripts\cuencas.gpkg"
+    morfoOutput=r'C:\Users\exequ\Documents\Contenedor_Scripts\morfoOutput.gpkg'
+
+
+    #Cálculo de Área en km2
+
+    areaVector=processing.run("native:fieldcalculator",
+    {'INPUT':cuencaInput,
+    'FIELD_NAME':'AREA_KM2',
+    'FIELD_TYPE':0,
+    'FIELD_LENGTH':0,
+    'FIELD_PRECISION':0,
+    'FORMULA':'$area /1000000',
+    'OUTPUT':'TEMPORARY_OUTPUT',
+    '--overwrite':True})
+
+    print("Area Terminada")
+        
+    #Cálculo del Perímetro en km
+
+    perimetroVector=processing.run("native:fieldcalculator",
+    {'INPUT':areaVector['OUTPUT'],
+    'FIELD_NAME':'PER_KM',
+    'FIELD_TYPE':0,
+    'FIELD_LENGTH':0,
+    'FIELD_PRECISION':0,
+    'FORMULA':' $perimeter /1000',
+    'OUTPUT':'TEMPORARY_OUTPUT',
+    '--overwrite':True})
+
+    print("Perimetro Terminado")
+
+    #Calculo de pendientes
+
+    pendiente=processing.run("native:slope", 
+    {'INPUT':dem,
+    'Z_FACTOR':1,
+    'OUTPUT':'TEMPORARY_OUTPUT',
+    '--overwrite':True})
+
+
+    print("Pendiente Terminada")
+
+
+     Estadistica zonal altitud
+
+    estAltitud=processing.run("native:zonalstatisticsfb", 
+    {'INPUT':perimetroVector['OUTPUT'],
+    'INPUT_RASTER':dem,
+    'RASTER_BAND':1,
+    'COLUMN_PREFIX':'ALT_',
+    'STATISTICS':[2,5,6],
+    'OUTPUT':'TEMPORARY_OUTPUT',
+    '--overwrite':True})
+
+    print("Estadistica Zonal Altitud Terminada")
+
+
+    #Estadstica zona pendiente
+
+    estPend=processing.run("native:zonalstatisticsfb", 
+    {'INPUT':estAltitud['OUTPUT'],
+    'INPUT_RASTER':pendiente['OUTPUT'],
+    'RASTER_BAND':1,
+    'COLUMN_PREFIX':'PEND_',
+    'STATISTICS':[2,5,6],
+    'OUTPUT':'TEMPORARY_OUTPUT',
+    '--overwrite':True})
+
+    print("Estadistica Zonal Pend.Terminada")
+
+
+    #Relieve de la Cuenca en Km
+
+    relieveVector=processing.run("native:fieldcalculator", 
+    {'INPUT':estPend['OUTPUT'],
+    'FIELD_NAME':'REL_Km',
+    'FIELD_TYPE':0,
+    'FIELD_LENGTH':0,
+    'FIELD_PRECISION':2,
+    'FORMULA':' "ALT_max" - "ALT_min" ',
+    'OUTPUT':'TEMPORARY_OUTPUT',
+    '--overwrite':True})
+
+    print("Relieve Terminado")
+
+    #Circularidad de la Cuenca
+
+    circCuenca=processing.run("native:fieldcalculator",
+    {'INPUT':relieveVector['OUTPUT'],
+    'FIELD_NAME':'Circ_Cuenca',
+    'FIELD_TYPE':0,
+    'FIELD_LENGTH':0,
+    'FIELD_PRECISION':2,
+    'FORMULA':'(4*pi()*"AREA_KM2")/("PER_KM")^2',
+    'OUTPUT':'TEMPORARY_OUTPUT',
+    '--overwrite':True})
+
+    print("Circulardad Terminada")
+
+    #Cálculo del número de rugosidad de Meltons
+
+    MRN=processing.run("native:fieldcalculator", 
+    {'INPUT':circCuenca['OUTPUT'],
+    'FIELD_NAME':'MRN',
+    'FIELD_TYPE':0,
+    'FIELD_LENGTH':0,
+    'FIELD_PRECISION':2,
+    'FORMULA':'("REL_Km")*("AREA_KM2"^-0.5)',
+    'OUTPUT':'TEMPORARY_OUTPUT',
+    '--overwrite':True})
+
+    print("Rugosidad Terminada")
+
+
+    #Clasificación Rugosidad
+
+    processing.run("native:fieldcalculator",
+    {'INPUT':MRN['OUTPUT'],
+     'FIELD_NAME': 'CLASS_MRN',
+        'FIELD_TYPE': 2,
+        'FIELD_LENGTH': 20, 
+        'FIELD_PRECISION': 0,
+        'FORMULA': "CASE WHEN \"MRN\" <= 1 THEN 'Superficie suave' "
+                   "WHEN \"MRN\" > 1 AND \"MRN\" <= 10 THEN 'Superficie moderada' "
+                   "WHEN \"MRN\" > 10 THEN 'Superficie rugosa' "
+                   "ELSE 'No definido' END",
+        'OUTPUT':morfoOutput,
+        '--overwrite': True
+    })
+
+    print("Clasificación de rugosidad terminada")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cuencastats.py
@@ -0,0 +1,118 @@
+def cuencastat(dem,cuencas,sizekm2):
+    
+    #Librerias
+    
+    import proccesing
+    import rasterio as rs
+    
+    #Entradas
+
+    dem=r"C:\Users\exequ\Documents\Contenedor_Scripts\DEM.tif"
+    cuencas=r"C:\Users\exequ\Documents\Contenedor_Scripts\cuencas.shp"
+    sizekm2=10
+
+    # Tamano de cuenca
+
+    demMeta=rs.open(dem)
+    resolucionArea= demMeta.res[0]*demMeta.res[1]
+    area=(sizekm2*1000000)/resolucionArea
+
+
+    #Delimitacion de cuenca
+
+    delimitCuenca=processing.run("grass7:r.watershed",
+        {'elevation':dem,
+        'depression':None,
+        'flow':None,
+        'disturbed_land':None,
+        'blocking':None,
+        'threshold':area,
+        'max_slope_length':None,
+        'convergence':5,
+        'memory':300,
+        '-s':False,
+        '-m':False,
+        '-4':False,
+        '-a':False,
+        '-b':False,
+        'accumulation':'TEMPORARY_OUTPUT',
+        'drainage':'TEMPORARY_OUTPUT',
+        'basin':'TEMPORARY_OUTPUT',
+        'stream':'TEMPORARY_OUTPUT',
+        'half_basin':'TEMPORARY_OUTPUT',
+        'length_slope':'TEMPORARY_OUTPUT',
+        'slope_steepness':'TEMPORARY_OUTPUT',
+        'tci':'TEMPORARY_OUTPUT',
+        'spi':'TEMPORARY_OUTPUT',
+        'GRASS_REGION_PARAMETER':None,
+        'GRASS_REGION_CELLSIZE_PARAMETER':0,
+        'GRASS_RASTER_FORMAT_OPT':'',
+        'GRASS_RASTER_FORMAT_META':'',
+        '--overwrite':True})
+
+
+    print("delimitacion_terminada")
+
+
+    #Poligonizar cuenca
+
+    poliCuenca=processing.run("gdal:polygonize", 
+        {'INPUT':delimitCuenca['basin'],
+        'BAND':1,
+        'FIELD':'DN',
+        'EIGHT_CONNECTEDNESS':False,
+        'EXTRA':'',
+        'OUTPUT':'TEMPORARY_OUTPUT',
+        '--overwrite':True})
+
+    print("poligonizacion terminada")
+
+
+    #Corregir geometria
+
+    cuencaVectorCorr=processing.run("native:fixgeometries",
+        {'INPUT':poliCuenca['OUTPUT'],
+        'METHOD':1,
+        'OUTPUT':'TEMPORARY_OUTPUT',
+        '--overwrite':True})
+
+    print("geometria_terminada")
+
+    #Calculo de pendientes
+
+    pendiente=processing.run("native:slope", 
+        {'INPUT':dem,
+        'Z_FACTOR':1,
+        'OUTPUT':'TEMPORARY_OUTPUT',
+        '--overwrite':True})
+
+    print("Pendiente Terminada")
+
+
+    #Estadistica zonal altitud
+
+    estAltitud=processing.run("native:zonalstatisticsfb", 
+        {'INPUT':cuencaVectorCorr['OUTPUT'],
+        'INPUT_RASTER':dem,
+        'RASTER_BAND':1,
+        'COLUMN_PREFIX':'Alt_',
+        'STATISTICS':[2,5,6],
+        'OUTPUT':'TEMPORARY_OUTPUT',
+        '--overwrite':True})
+
+    print("Estadistica Zonal Altitud Terminada")
+
+
+    #Estadstica zona pendiente
+
+    processing.run("native:zonalstatisticsfb", 
+        {'INPUT':estAltitud['OUTPUT'],
+        'INPUT_RASTER':pendiente['OUTPUT'],
+        'RASTER_BAND':1,
+        'COLUMN_PREFIX':'Pen_',
+        'STATISTICS':[2,0,0],
+        'OUTPUT':cuencas,
+        '--overwrite':True})
+
+    print("Estadistica Zonal Pend.Terminada")
+
